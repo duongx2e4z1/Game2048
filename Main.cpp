87d8651e2a27b73238ba3e2d77538f3272b0d76c
duongx2e4z1 <thaiduong003@gmail.com>
2021-03-16 20:23:35 +0700

    16/3

--- Main.cpp
+++ Main.cpp
@@ -48,11 +48,88 @@ void spawnNumber()
                 }
                 --vt;
             }
+
+    if (vt != 0)
+        exit(0);
+}
+
+void changeLR()
+{
+    for (int i=0; i<sizeOfBoard; ++i)
+        for (int j=0;j<sizeOfBoard/2; ++j)
+            swap(a[i][j], a[i][sizeOfBoard-j-1]);
+}
+
+void changeLU()
+{
+    for (int i=0; i<sizeOfBoard; ++i)
+        for (int j=i+1; j<sizeOfBoard; ++j)
+            swap(a[i][j], a[j][i]);
+}
+
+void lef()
+{
+    for (int i=0; i<sizeOfBoard; ++i)
+    {
+        deque <int> dq;
+        for (int j=0; j<sizeOfBoard; ++j)
+        {
+            if (a[i][j] == 0)
+                continue;
+            if (!dq.empty() && dq.back() == a[i][j])
+            {
+                dq.pop_back();
+                dq.push_back(a[i][j] * 2);
+            }
+            else
+                dq.push_back(a[i][j]);
+        }
+        for (int j=0; j<sizeOfBoard; ++j)
+            if (!dq.empty())
+            {
+                a[i][j] = dq.front();
+                dq.pop_front();
+            }
+            else
+                a[i][j] = 0;
+    }
+}
+
+void rig()
+{
+    changeLR();
+    lef();
+    changeLR();
+}
+
+void up()
+{
+    changeLU();
+    lef();
+    changeLU();
+}
+
+void down()
+{
+    changeLU();
+    rig();
+    changeLU();
+}
+
+void getmove(char c)
+{
+    if (c == 'a')
+        lef();
+    if (c == 'd')
+        rig();
+    if (c == 'w')
+        up();
+    if (c == 's')
+        down();
 }
 
 void gameplay()
 {
-
     fill_n(&a[0][0], sizeOfBoard * sizeOfBoard, 0);
     srand(time(0));
     char c;
@@ -61,7 +138,7 @@ void gameplay()
         srcClear();
         spawnNumber();
         outBoard();
-
+        getmove(c);
     }
 }
 
